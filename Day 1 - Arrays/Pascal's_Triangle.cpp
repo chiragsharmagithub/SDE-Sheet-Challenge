a0a7536e0e61b0f912c4f7772fd7557d2a96dd41
Chirag Sharma <chiragsharmaindian@gmail.com>
2022-06-06 14:27:45 +0530

    Added Pascal's triangle question
    Contains question link, logic, c++ code.

---
+++ Day 1 - Arrays/Pascal's_Triangle.cpp
@@ -0,0 +1,49 @@
+// Question link
+// https://leetcode.com/problems/pascals-triangle/
+
+// Logic:
+
+/*
+
+In each row, the first and last element will be 1
+Other elements, are the sum of previous row's (j)th and (j-1)th elements
+Basically, Fundation of DP, we use previous values to evaluate current element's values
+
+result[i][j] = result[i-1][j-1] + result[i-1][j];
+
+*/
+
+// Chirag
+// TC: O(N^2)
+// SC: O(1)
+
+// where N = numRows
+// While O(numRows^2)O(numRows)2
+//  ) space is used to store the output, the input and output generally do not count towards the space complexity. 
+
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> result;
+        
+        for(int i=0; i<numRows; i++) {
+            vector<int> tempArr;
+            for(int j=0; j<=i; j++) {
+                if(j == 0) {
+                    tempArr.push_back(1);
+                }
+                else if(j == i) {
+                    tempArr.push_back(1);
+                }
+                // This will contain all the elements other than starting and ending element of each row
+                else {
+                    int sum = result[i-1][j-1] + result[i-1][j];
+                    tempArr.push_back(sum);
+                }
+            }
+            result.push_back(tempArr);
+        }
+        return result;
+    }
+};
